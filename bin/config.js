f485346eef06fbd9871e1785b589cefdce8561f0
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-07-02 15:51:37 +0300

    autorestoer TB config if broken

--- bin/config.js
+++ bin/config.js
@@ -2,24 +2,25 @@ const path = require('path');
 const fs = require('fs');
 const ConfigFilePath = path.join(__dirname, '..', 'config.json');
 
+let defaultConfig = {
+    branch: 'master',
+    uilanguage: 'en',
+    updatelog: false,
+    devmode: false,
+    noselfupdate: false,
+    noupdate: false,
+    noslstags: false,
+    noserverautojoin: false
+};
+
 function loadConfig() {
     let result = null;
     try {
         result = fs.readFileSync(ConfigFilePath, 'utf8');
+        return JSON.parse(result);
     } catch (_) {
-        return {
-            branch: 'master',
-            uilanguage: 'en',
-            updatelog: false,
-            devmode: false,
-            noselfupdate: false,
-            noupdate: false,
-            noslstags: false,
-            noserverautojoin: false
-        };
+        return defaultConfig;
     }
-
-    return JSON.parse(result);
 }
 
 function saveConfig(newConfig) {
